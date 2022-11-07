eb36fbf2c80b8ccb4278047d7ebca7fd48e824c4
ssastre <sandra_sastre@hotmail.com>
2022-11-06 19:00:22 -0600

    Revert "Revert "arreglamos loop""
    This reverts commit ee3c7ce8066a93aff37a2c44ed384ec4f8a12afe.

--- app/screen/ResultadosTest.js
+++ app/screen/ResultadosTest.js
@@ -13,32 +13,19 @@ export default function LoginScreen(props){
 
         return(
             <View style={[mainStyles.container,{paddingTop: 10}]}>
-                <Text style={ [mainStyles.unoText,{fontSize: 53}]}>Resultados Test</Text>
-                {/* <Text style={ [mainStyles.unoText,{bottom:35,fontSize: 53 }]}>DALTONE</Text> */}
+                <Text style={ [{color: 'black',fontSize: 53}]}>Resultados</Text>
+                <Text style={ [{color: 'black',fontSize: 53}]}>Test</Text>
 
-                <View style={[loginStyles.logo, {bottom:50}]}>
-                {/* <Image source={require('@recursos/images/64943.png')}
-                    style={{ height: 250, width: 250 }} /> */}
-            </View>
-            <Text style={ [mainStyles.unoText,{bottom:80, fontSize: 25}]}>Te ayudaremos a</Text>
-            <Text style={ [mainStyles.unoText,{bottom:120, fontSize: 25}]}> distinguir los colores</Text>
+                
 
-        <View style={[mainStyles.otro2Btn, {bottom:100}]}>
+        <View style={[mainStyles.otro2Btn, {top:120} ]}>
             <TouchableOpacity
                 onPress={() => goToScreen('Register')}>
                 <Text style={mainStyles.dosText}>¿Empezamos?</Text>
             </TouchableOpacity>
         </View>
 
-        <View style={[{bottom:60}]}>
-        <Text style={[ {right:70, color:'black', fontSize: 22 }, { textDecorationLine: 'underline' }, { textAlign: 'center' }, { paddingTop: 25 }, { bottom: 27 }, {}]}>¿Ya tenes cuenta?</Text>
-
-            <TouchableOpacity
-          onPress={() => goToScreen('Inicio')}>
-            <Text style={[{ left:89, color: color.LBLUE, fontSize: 22  }, { textDecorationLine: 'underline' }, { textAlign: 'center' }, { paddingTop: 25 }, { bottom: 81.4 }, {}]}>Inicia sesion</Text>
-
-        </TouchableOpacity>
-        </View>
+    
         
 
             </View>
